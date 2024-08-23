451f6a4c0f2fbb8feffdb2183c430804a50d2ede
Praveen851 <rpraveenbabu851@gmail.com>
2024-08-24 00:22:45 +0530

    feat: create draggable balls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const balls = document.querySelectorAll(".ball-container");
+
+        function onDragStart() {
+            setTimeout(
+                () => setTimeout(() => this.classList.add("invicible"), 0),
+                0
+            );
+        }
+
+        function onDragEnd(e) {
+            this.classList.remove("dragging");
+            this.style.position = "absolute";
+            this.style.top = e.clientY - 82 + "px";
+            this.style.left = e.clientX - 35 + "px";
+        }
+        if (balls) {
+            balls.forEach((ball) => {
+                ball.addEventListener("dragstart", onDragStart);
+                ball.addEventListener("dragend", onDragEnd);
+            });
+        }
+
+        document
+            .querySelector(".App")
+            .addEventListener("dragover", function (e) {
+                e.preventDefault();
+            });
+
+        return () => {
+            if (balls) {
+                balls.forEach((ball) => {
+                    ball.removeEventListener("dragstart", onDragStart);
+                    ball.removeEventListener("dragend", onDragEnd);
+                });
+            }
+        };
+    }, []);
+
+    return (
+        <div className="App">
+            <div id="red-ball" className="ball-container" draggable></div>
+            <div id="green-ball" className="ball-container" draggable></div>
+            <div id="blue-ball" className="ball-container" draggable></div>
+            <div id="purple-ball" className="ball-container" draggable></div>
+            <div id="pink-ball" className="ball-container" draggable></div>
+        </div>
+    );
 }
 
 export default App;
