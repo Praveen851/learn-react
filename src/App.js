e487e2930b7075d5be5a474789fa68ec5b4b1f58
Praveen851 <rpraveenbabu851@gmail.com>
2024-08-25 16:56:19 +0530

    title: flushSync example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { flushSync } from "react-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /* 
+    Concept of automatic batching in React 18.
+  Batching is a react feature that existed even before react 18, it helps us to avoid multiple re-renders for each state change inside a single event handler function. 
+  Inside an event handler it waits until all the state updates are done and then it re-renders the ui.
+  It is limited to event handlers alone. It doesn’t batch the state updates inside timeouts, promises, etc.
+  With React 18, updates inside of promises, setTimeout, native event handlers, or any other event are getting batched automatically. And the best part is that you don’t have to add anything to your code to use automatic batching, React does it automatically after you upgrade your project’s React version to 18.
+  React introduces flushSync() function to stop automatic batching when necessary. You can import it from react-dom 
+  reference : `https://javascript.plainenglish.io/react-18-automatic-batching-3cf7bf545ab6`
+  */
+    const [name, setName] = useState("");
+    const [clicked, setClicked] = useState(false);
+
+    console.log("Rerendered", clicked, name); //check console after clicking the button
+
+    const handleOnclick = () => {
+        flushSync(() => setName("old")); // here flushSync is used to stop automatic batching hence after clicking the button the component is rendered two times( flush sync immediately updates states and re-renders the component without automatic batching)
+        setClicked((prev) => !prev);
+    };
+
+    return (
+        <div>
+            <button onClick={handleOnclick}>Click</button>
+            <input onChange={(event) => setName(event.target.value)}></input>
+            <div>name {name} </div>
+        </div>
+    );
 }
 
 export default App;
