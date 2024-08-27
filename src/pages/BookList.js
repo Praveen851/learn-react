0180f0c8965e4939e87d939c408e9feae6533893
Praveen851 <rpraveenbabu851@gmail.com>
2024-08-27 16:34:27 +0530

    Lesson3: Dynamic routes

--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+
 export function BookList() {
-    return <h1>BookList</h1>;
+    return (
+        <>
+            <h1>BookList</h1>
+            <ul>
+                <li>
+                    <Link to={"/book/1"}>book1</Link>
+                </li>
+                <li>
+                    <Link to={"/book/2"}>book2</Link>
+                </li>
+                <li>
+                    <Link to={"/book/3"}>book3</Link>
+                </li>
+            </ul>
+        </>
+    );
 }
