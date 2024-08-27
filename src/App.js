416ec18b46a73c4b739b24a0e815555768715956
Praveen851 <rpraveenbabu851@gmail.com>
2024-08-27 19:33:09 +0530

    Lesson7: Link tag and its props

--- src/App.js
+++ src/App.js
@@ -32,10 +32,15 @@ function App() {
 
 /* 
 Note:
-1) use index prop to set the component for the root path
+Lesson  5:
+1) use index prop to set the component for the root path.
 2) You can remove the path of the parent "/books" then we can use it wrap any components without a common parent. Useful for showing navbar.
 3) To show extra content use route component to render multiple element for the same route.
-4) Use location prop in the routes to hardcode the current location 
+4) Use location prop in the routes to hardcode the current location.
+Lesson 7:
+5) Link tag -> `replace` prop to replace the current component with the new navigated component.
+6) Link tag -> `reloadDocument` prop to reload and render the new component.
+7) Link tag -> `state` prop to pass data that wont shown in url bar.
 */
 
 export default App;
