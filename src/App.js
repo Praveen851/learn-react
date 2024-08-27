ca4de75e765c41bdcdaaa19bf2d3302bd67ec379
Praveen851 <rpraveenbabu851@gmail.com>
2024-08-27 13:05:36 +0530

    Lesson 1: Basics of react-router-dom
    1) Import BrowserRouter from react-router-dom and wrap ur app component with it.
    2) Define all your routes inside Routes component
    3) Use link tags to move/redirect around the application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { BookList } from "./pages/BookList";
-import { Book } from "./pages/Book";
 
 function App() {
     return (
@@ -19,7 +18,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/books" element={<BookList />} />
-                <Route path="/book/:id" element={<Book />} />
             </Routes>
         </>
     );
