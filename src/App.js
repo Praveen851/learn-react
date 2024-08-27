b4698141290f76b9777dc7b94f880ae36cb7b73b
Praveen851 <rpraveenbabu851@gmail.com>
2024-08-27 16:42:59 +0530

    Lesson4: Route Specificity

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Link, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { BookList } from "./pages/BookList";
 import { Book } from "./pages/Book";
+import { NewBook } from "./pages/NewBook";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
     return (
@@ -20,9 +22,18 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/books" element={<BookList />} />
                 <Route path="/book/:id" element={<Book />} />
+                <Route path="/book/new" element={<NewBook />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </>
     );
 }
+
+/* 
+Note:
+1) Prior to react router 6 /book/new would have also treated it as dynamic and hence <Book/> component would have been rendered
+instead of <NewBook/> component based on the order 
+2) when the path is set "*" it matches to anything in the url, so we can use it to render 404 page by placing it at last.
+*/
 
 export default App;
