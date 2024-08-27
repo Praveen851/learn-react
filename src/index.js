3b1598ed82135c6286d8f33303c16f635720a462
Praveen851 <rpraveenbabu851@gmail.com>
2024-08-27 16:30:58 +0530

    Lesson 1: Basics of react-router-dom
    1) Import BrowserRouter from react-router-dom and wrap ur app component with it.
    2) Define all your routes inside Routes component
    3) Use link tags to move/redirect around the application

--- src/index.js
+++ src/index.js
@@ -12,13 +12,3 @@ root.render(
         </BrowserRouter>
     </React.StrictMode>
 );
-
-// https://www.youtube.com/watch?v=Ul3y1LXxzdU
-
-/* 
-othere router
-HashRouter -> main url remains the same, other changes in the url are not considered as a part of the url rather it is considered as a hash.
-unStable_HistoryRouter -> allows us to take control over the history element of react-router. Provide us direct access to manipulate as we need.
-MemoryRouter -> stores the path in memory instead of changing in the url. Used to run test.
-StaticRouter -> specifically for working on the server. Hardcodes the url in the browser.
-*/
