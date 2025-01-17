db490ac825ce0c620f2b43d6fd85e249456edc6a
Praveen851 <rpraveenbabu851@gmail.com>
2025-01-17 16:09:50 +0530

    hoc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-    return <></>;
+// Higher-Order Component
+const withLoading = (WrappedComponent) => {
+  return ({ isLoading, ...props }) => {
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+    return <WrappedComponent {...props} />;
+  };
+};
+
+// Regular Component
+const DataDisplay = ({ data }) => {
+  return (
+    <ul>
+      {data.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
+// Enhanced Component with HOC
+const DataDisplayWithLoading = withLoading(DataDisplay);
+
+// Usage
+export default function App() {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setData(["Item 1", "Item 2", "Item 3"]);
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  return <DataDisplayWithLoading isLoading={isLoading} data={data} />;
 }
-
-export default App;
