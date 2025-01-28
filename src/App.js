bc64407acc362e9dbe397bbaa3d2dbb7b29be3eb
Praveen851 <rpraveenbabu851@gmail.com>
2025-01-28 11:18:00 +0530

    implement useDebounce hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import useDebounce from "./useDebounce";
 import "./App.css";
 
 function App() {
-    return <></>;
+    const [value, setValue] = useState("");
+    const debouncedValue = useDebounce(value, 1000);
+    useEffect(() => console.log(debouncedValue), [debouncedValue]);
+
+    return (
+        <div className="container">
+            <input
+                placeholder="Enter text"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
+        </div>
+    );
 }
 
 export default App;
